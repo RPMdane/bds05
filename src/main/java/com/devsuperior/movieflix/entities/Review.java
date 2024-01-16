858f92417a8a6d5c5a44be30ecc1e43231b324d4
RPMdane <muendane@gmail.com>
2024-01-16 17:13:33 +0200

    Review

--- src/main/java/com/devsuperior/movieflix/entities/Review.java
+++ src/main/java/com/devsuperior/movieflix/entities/Review.java
@@ -6,25 +6,30 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-
 @Entity
-@Table(name = "tb_role")
-public class Role implements Serializable {
+@Table(name = "tb_review")
+public class Review implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String authority;
+    private String text;
 
-    public Role() {
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Review() {
     }
 
-    public Role(Long id, String authority) {
+    public Review(Long id, String text) {
         this.id = id;
-        this.authority = authority;
+        this.text = text;
     }
 
     public Long getId() {
@@ -35,12 +40,12 @@ public class Role implements Serializable {
         this.id = id;
     }
 
-    public String getAuthority() {
-        return authority;
+    public String getText() {
+        return text;
     }
 
-    public void setAuthority(String authority) {
-        this.authority = authority;
+    public void setText(String text) {
+        this.text = text;
     }
 
     @Override
@@ -59,7 +64,7 @@ public class Role implements Serializable {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        Role other = (Role) obj;
+        Review other = (Review) obj;
         if (id == null) {
             if (other.id != null)
                 return false;
--- src/main/java/com/devsuperior/movieflix/entities/User.java
+++ src/main/java/com/devsuperior/movieflix/entities/User.java
@@ -1,6 +1,9 @@
 package com.devsuperior.movieflix.entities;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Entity;
@@ -11,11 +14,13 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,6 +35,9 @@ public class User {
         inverseJoinColumns = @JoinColumn(name = "role_id")
     )
     private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Review> reviews = new ArrayList<>();
 
     public User() {
     }
